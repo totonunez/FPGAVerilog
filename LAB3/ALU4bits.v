071e7fbc42739fbe17499bf91fb399ccc5214c79
totonunez <39102145+totonunez@users.noreply.github.com>
2018-11-12 19:50:19 -0300

    actualización de código Verilog
    se a logrado crear nuevamente las cosas

--- LAB3/ALU4bits.v
+++ LAB3/ALU4bits.v
@@ -1,12 +1,14 @@
 module ALU4bits(
 	input [9:0] SW,
+	input [3:0] A,B,
+	input [2:0] LogicOperation,
 	output [9:0] LED,
 	output reg [7:0] AN,
-    output reg [6:0] OUT
+        output reg [6:0] OUT
 	);
 	reg [3:0] DISPLAY; // Numero que mostrara el display.Cantidad de Bits por que por display
 	reg [26:0] CONTADOR_SEG; // Contador para generar segundos segun el CLK de la placa. 134.217.728 (ms) para generar CLK
-    wire SEGUNDO; // Indicador que paso un segundo.
+        wire SEGUNDO; // Indicador que paso un segundo.
     
 	assign LED[0] = SW[0];
 	assign LED[1] = SW[1];
@@ -19,37 +21,57 @@ module ALU4bits(
 	assign LED[8] = SW[8];
 	assign LED[9] = SW[9];
 	
+    always @(posedge CLK100MHZ or posedge RST)
+	begin
+		if(RST==1)
+			CONTADOR_SEG <= 0;
+		else begin
+			if(CONTADOR_SEG>=99999999) 
+                 CONTADOR_SEG <= 0;
+            else
+                CONTADOR_SEG <= CONTADOR_SEG + 1;
+		end
+	end
+	assign SEGUNDO = (CONTADOR_SEG==99999999)?1:0;
+	
+	
     always @(posedge CLK100MHZ)
-    
-    end
+	    begin
+		    case(LogicOperation)
+			4'b00: // Suma	
+			        DISPLAY = A + B ;
+        		4'b01: // Subtraction
+				if(A > B)
+					DISPLAY = A - B ;
+			        else
+					DISPLAY = -(A-B)
+			4'b10: // Multiplication
+				DISPLAY = A * B;
+			4'b11: // Division
+				DISPLAY = A/B;
+			default : DISPLAY = A+B
+		    endcase  
+	    end
     
 always @(*)
     begin
         case(DISPLAY)
-        4'b0000: OUT[0] = 7'b0000001; // "0"     
-        4'b0001: OUT[0] = 7'b1001111; // "1" 
-        4'b0010: OUT[0] = 7'b0010010; // "2" 
-        4'b0011: OUT[0] = 7'b0000110; // "3" 
-        4'b0100: OUT[0] = 7'b1001100; // "4" 
-        4'b0101: OUT[0] = 7'b0100100; // "5" 
-        4'b0110: OUT[0] = 7'b0100000; // "6" 
-        4'b0111: OUT[0] = 7'b0001111; // "7" 
-        4'b1000: OUT[0] = 7'b0000000; // "8"     
-        4'b1001: OUT[0] = 7'b0000100; // "9"
-        4'b1010: OUT[0] = 7'b1001111; // "10"
-        4'b1011: OUT[0] = 7'b1001111; // "11" 
-        4'b1100: OUT[0] = 7'b1001111; // "12" 
-        4'b1101: OUT[0] = 7'b1001111; // "13" 
-        4'b1110: OUT[0] = 7'b1001111; // "14" 
-        4'b1111: OUT[0] = 7'b1001111; // "15"  
+		for (i=0;i<4;i++)
+			begin
+				4'b0000: OUT[i] = 7'b0000001; // "0"     
+				4'b0001: OUT[i] = 7'b1001111; // "1" 
+				4'b0010: OUT[i] = 7'b0010010; // "2" 
+				4'b0011: OUT[i] = 7'b0000110; // "3" 
+				4'b0100: OUT[i] = 7'b1001100; // "4" 
+				4'b0101: OUT[i] = 7'b0100100; // "5" 
+				4'b0110: OUT[i] = 7'b0100000; // "6" 
+				4'b0111: OUT[i] = 7'b0001111; // "7" 
+				4'b1000: OUT[i] = 7'b0000000; // "8"     
+				4'b1001: OUT[i] = 7'b0000100; // "9"
+				4'b1001: OUT[i] = 7'b1111110; // "-"
+				default: OUT[i] = 7'b0000001; // "0"
+			end
         
-        4'b1010: OUT[1] = 7'b0000001; // "0"     
-        4'b1011: OUT[1] = 7'b1001111; // "1"
-        4'b1100: OUT[1] = 7'b0010010; // "2"
-        4'b1101: OUT[1] = 7'b0000110; // "3"
-        4'b1110: OUT[1] = 7'b1001100; // "4"
-        4'b1111: OUT[1] = 7'b0100100; // "5"
-        default: OUT = 7'b0000001; // "0"
         endcase
         AN = 8'b11111110; // El display que está activo será el último dado que es el que está en cero
     end
