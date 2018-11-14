af4179f92cd2683a1ee06a7934c6b152fda0329d
totonunez <39102145+totonunez@users.noreply.github.com>
2018-11-14 17:32:47 -0300

    Actualización de ALU 4 bits
    Realizar sumas, restas, división y multiplicación de bits.

--- LAB3/ALU4bits.v
+++ LAB3/ALU4bits.v
@@ -1,78 +1,100 @@
-module ALU4bits(
-	input [9:0] SW,
-	input [3:0] A,B,
-	input [2:0] LogicOperation,
-	output [9:0] LED,
-	output reg [7:0] AN,
-        output reg [6:0] OUT
-	);
-	reg [3:0] DISPLAY; // Numero que mostrara el display.Cantidad de Bits por que por display
-	reg [26:0] CONTADOR_SEG; // Contador para generar segundos segun el CLK de la placa. 134.217.728 (ms) para generar CLK
-        wire SEGUNDO; // Indicador que paso un segundo.
+
+module ALU(
+    input CLK100MHZ,
+    input [3:0] Num1, Num2,
+    input [1:0] Op,
+    output [3:0] LED1, LED2,
+    output [1:0] LED_Op,
+    output reg [7:0] AN,
+    output reg [6:0] display
+    );
+    reg [7:0] result;
+    reg [3:0] digito;
+    reg [18:0] refresh;
+    wire [2:0] active_display;
     
-	assign LED[0] = SW[0];
-	assign LED[1] = SW[1];
-	assign LED[2] = SW[2];
-	assign LED[3] = SW[3];
-	assign LED[4] = SW[4];
-	assign LED[5] = SW[5];
-	assign LED[6] = SW[6];
-	assign LED[7] = SW[7];
-	assign LED[8] = SW[8];
-	assign LED[9] = SW[9];
-	
-    always @(posedge CLK100MHZ or posedge RST)
-	begin
-		if(RST==1)
-			CONTADOR_SEG <= 0;
-		else begin
-			if(CONTADOR_SEG>=99999999) 
-                 CONTADOR_SEG <= 0;
-            else
-                CONTADOR_SEG <= CONTADOR_SEG + 1;
-		end
-	end
-	assign SEGUNDO = (CONTADOR_SEG==99999999)?1:0;
-	
-	
-    always @(posedge CLK100MHZ)
-	    begin
-		    case(LogicOperation)
-			4'b00: // Suma	
-			        DISPLAY = A + B ;
-        		4'b01: // Subtraction
-				if(A > B)
-					DISPLAY = A - B ;
-			        else
-					DISPLAY = -(A-B)
-			4'b10: // Multiplication
-				DISPLAY = A * B;
-			4'b11: // Division
-				DISPLAY = A/B;
-			default : DISPLAY = A+B
-		    endcase  
-	    end
+    assign LED1 = Num1;
+    assign LED2 = Num2;
+    assign LED_Op = Op;
     
-always @(*)
+    always @(*)
     begin
-        case(DISPLAY)
-		for (i=0;i<4;i++)
-			begin
-				4'b0000: OUT[i] = 7'b0000001; // "0"     
-				4'b0001: OUT[i] = 7'b1001111; // "1" 
-				4'b0010: OUT[i] = 7'b0010010; // "2" 
-				4'b0011: OUT[i] = 7'b0000110; // "3" 
-				4'b0100: OUT[i] = 7'b1001100; // "4" 
-				4'b0101: OUT[i] = 7'b0100100; // "5" 
-				4'b0110: OUT[i] = 7'b0100000; // "6" 
-				4'b0111: OUT[i] = 7'b0001111; // "7" 
-				4'b1000: OUT[i] = 7'b0000000; // "8"     
-				4'b1001: OUT[i] = 7'b0000100; // "9"
-				4'b1001: OUT[i] = 7'b1111110; // "-"
-				default: OUT[i] = 7'b0000001; // "0"
-			end
-        
+        case(Op)
+        2'b00: // Suma
+            result = Num1 + Num2;
+        2'b01: // Resta
+            result = (Num1 > Num2)?(Num1 - Num2):(Num2 - Num1);
+        2'b10: // Multiplicacion
+            result = Num1 * Num2;
+        2'b11: // Division
+            result = Num1 / Num2;    
+        default:
+            result = Num1 + Num2;
         endcase
-        AN = 8'b11111110; // El display que está activo será el último dado que es el que está en cero
+    end
+    
+    always @(posedge CLK100MHZ)
+    begin
+        if(refresh >= 500000)
+            refresh <= 0;
+        else
+            refresh <= refresh + 1;
+    end
+    assign active_display = refresh[18:16];
+    
+    always @(*)
+    begin
+        case(active_display)
+            3'b000: begin
+                AN = 8'b11111110;
+                digito = result%100%10;
+                   end
+            3'b001: begin
+                AN = 8'b11111101;
+                digito = result%100/10;    
+                   end
+            3'b010: begin                                
+                AN = 8'b11111011;
+                digito = result/100;
+                   end
+            3'b011: begin
+                AN = 8'b11110111;
+                digito = Op;
+                   end
+            3'b100: begin
+                AN = 8'b11101111;
+                digito = Num2%100%10;
+                   end 
+            3'b101: begin
+                AN = 8'b11011111;
+                 digito = Num2%100/10;
+                   end                    
+            3'b110: begin
+                 AN = 8'b10111111;
+                 digito = Num1%100%10;
+                   end  
+            3'b111: begin
+                 AN = 8'b01111111;
+                 digito = Num1%100/10;
+                   end                            
+        endcase
+    end
+    
+    always @(*)
+    begin
+        case(digito)
+        4'b0000: display = 7'b0000001; // "0"     
+        4'b0001: display = 7'b1001111; // "1" 
+        4'b0010: display = 7'b0010010; // "2" 
+        4'b0011: display = 7'b0000110; // "3" 
+        4'b0100: display = 7'b1001100; // "4" 
+        4'b0101: display = 7'b0100100; // "5" 
+        4'b0110: display = 7'b0100000; // "6" 
+        4'b0111: display = 7'b0001111; // "7" 
+        4'b1000: display = 7'b0000000; // "8"     
+        4'b1001: display = 7'b0000100; // "9"
+        4'b1010: display = 7'b1111110; // "-"
+        default: display = 7'b0000001; // "0"
+        endcase 
     end
 endmodule
